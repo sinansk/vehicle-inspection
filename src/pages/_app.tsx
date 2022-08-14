a60a7a141388f94d789b6cad610bf91e5143d73b
Sinan Şık <83316099+sinansk@users.noreply.github.com>
2022-08-14 20:54:09 +0300

    fixed-routing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,6 @@ import { AppProps } from "next/app";
 import "../styles/index.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Register from "../components/Register";
-import ContactPage from "./ContactPage";
-import AboutUsPage from "./AboutUsPage";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -15,9 +12,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Header />
-      {/* <Register /> */}
-      {/* <AboutUsPage /> */}
-      {/* <ContactPage /> */}
       <Component {...pageProps} />
       <Footer />
     </>
